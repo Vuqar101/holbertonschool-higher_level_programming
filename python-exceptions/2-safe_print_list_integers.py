d2cba833c77b6a1f7867dd33bb9da9d92941f2d5
Vuqar101 <veliyevvuqar10@gmail.com>
2026-04-18 16:33:23 +0000

    salam

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for i in range(x):
+        value = my_list[i]
+        try:
+            print("{:d}".format(value), end="")
+            count += 1
+        except (ValueError, TypeError):
+            continue
+    print()
+    return count
